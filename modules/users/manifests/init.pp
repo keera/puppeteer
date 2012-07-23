54ad40e9957e4a7405e28d0bbda9ffb23926c165
root <alin@2tor.com>
2012-07-23 14:20:36 +0000

    moved group maps to virtualteam

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -5,31 +5,29 @@ class users{
     include usergroups
 
     #create clientside user groups
-    $dev_title = "developers"
-    $dev_gid   = 2002
-    virtualteam::localteam{"$dev_title"
-        gid => $dev_gid,
+    virtualteam::localteam{$virtualteam::development[title]:
+        gid => $virtualteam::development[id],
     }
 
     #create clientside users
     virtualuser::localuser {'john':
         uid     => 2000,
-        gid     => $dev_gid,
-        team    => $dev_title,
+        gid     => $virtualteam::development[id],
+        team    => $virtualteam::development[title],
         pass    => 'harhar',
-        groups  => [$usergroups::group[none]],
-        sshkey  => 'abcdeSSHKEYfg',
-        require => Virtualteam::Localteam[$dev_title],
+        groups  => [$usergroups::group[all]],
+        sshkey  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDHV5JEHtAVrK2JuBeBneUKe7CSRiKt44Cb+Y89CZWvXe8pTTQAy1I54X78ghivur5ebs/VnYEPzty/o06FiIZc4ax25hve0T41VLlsnAmcUigt8Fdiiiq7O1ZBCPqg9HF4338vIq/u6DjdRSci7XW2YEa3zWSYjIfmLl6gaGI26+h7dKOqwyOR7baw8SHgTVXtUxGiRKGZXC4bQwBNLOFPhGNLbUKVF8TDtQBLzz/zEoTLIrHZJBi/SQOckj03yh+tfBa9UhkpgP6ZJeKfB0bq1cSmX/tocBeVs3wr+mftzOgBsT4QO4mSGgq2It5jxnx1GXgiJY0VZkXEdUISkYAt',
+        require => Virtualteam::Localteam[$virtualteam::development[title]],
     }
 
     virtualuser::localuser {'joe':
         uid     => 2001,
-        gid     => $dev_gid,
-        team    => $dev_title,
+        gid     => $virtualteam::development[id],
+        team    => $virtualteam::development[title],
         pass    => 'harhar',
-        groups  => [$usergroups::group[none]],
+        groups  => [$usergroups::group[true_test]],
         sshkey  => '1234SSHKEYfg',
-        require => Virtualteam::Localteam[$dev_title],
+        require => Virtualteam::Localteam[$virtualteam::development[title]],
     }
 
 }
--- modules/users/manifests/virtualteam.pp
+++ modules/users/manifests/virtualteam.pp
@@ -1,5 +1,20 @@
 class virtualteam{
 
+    $development = {
+        id   => 2002,
+        title => 'developers',
+    }
+
+    $assurance = {
+        id   => 2003,
+        title => 'qa',
+    }
+
+    $production = {
+        id   => 2004,
+        title => 'production',
+    }
+
     define localteam($gid){
 
         group { "$title":
